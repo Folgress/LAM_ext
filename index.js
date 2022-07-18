cdcbc19e8c190ac03bce640d9236841da7e8d11a
Folgress <75598739+Folgress@users.noreply.github.com>
2022-07-18 21:12:03 +0200

    update relative url to absolute

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
-fetch("./data.json").then(res => res.json()).then(json => {
+const url = "https://raw.githubusercontent.com/Folgress/LAM_ext/develope/";
+fetch(url + "data.json").then(res => res.json()).then(json => {
     console.log("Running " + json.name + " v." + json.version);
 })
