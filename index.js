4f702b6daf608c9567f744563fe0718b917bf88b
Folgress <75598739+Folgress@users.noreply.github.com>
2022-07-18 22:26:29 +0200

    display version

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const url = "https://raw.githubusercontent.com/Folgress/LAM_ext/develope/";
 fetch(url + "data.json").then(res => res.json()).then(json => {
-    console.log("Running " + json.name + " v." + json.version);
+    console.log("Running " + json.name + " v" + json.version.major + "." + json.version.minor);
 })
