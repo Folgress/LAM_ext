58b4294cbb298f1fefd150046eae4943b66973f6
Folgress <75598739+Folgress@users.noreply.github.com>
2022-07-19 22:07:17 +0200

    updated getJS()

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ let s = document.createElement("script"); s.src = "https://folgress.github.io/LA
 
 const url = "https://raw.githubusercontent.com/Folgress/LAM_ext/develope/";
 const timer = 10000;
+let currentHashCode = null;
 
 fetch(url + "data.json").then(res => res.json()).then(json => {
     console.info("Running " + json.name + " v" + json.version.major + "." + json.version.minor);
@@ -12,13 +13,26 @@ fetch(url + "data.json").then(res => res.json()).then(json => {
 })
 
 function getJS() {
-    console.log("Fetching getMerchant...")
+    console.log("Fetching getMerchant ...")
 
     fetch(url + "getMerchant.js").then(res => res.text()).then(js => {
+        console.log("... file fetched, compute hash ...")
 
+        const newHash = js.hashCode();
+
+        if (currentHashCode !== newHash) {
+            console.log("... hash unequal, pushing new script ...");
+
+            currentHashCode = newHash;
+
+            // TODO: push new script and remove old one, if exitst!
+        } else {
+            console.log("... hash equal, do nothing!")
+        }
+
+
+        setTimeout(getJS, timer);
     })
-
-    setTimeout(getJS, timer);
 }
 
 String.prototype.hashCode = function () {
